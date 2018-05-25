8d7cfbe1fac91adaa2bd9e1db054bedb3f53a46a
Mitchell <mitch.ohara-wild@monash.edu>
2018-05-25 13:43:34 +1000

    Finished feature wall

--- gen_wall.R
+++ gen_wall.R
@@ -3,7 +3,7 @@ library(magick)
 library(purrr)
 
 # Params
-sticker_row_size = 10 # The number of stickers in the first row
+sticker_row_size = 10 # The number of stickers in the longest row
 sticker_width = 200 # The width of each sticker in pixels
 
 # Script
@@ -33,6 +33,7 @@ stickers <- stickers[!sticker_rm]
 message(sprintf("Automatically removed %i incompatible stickers: %s",
                 sum(sticker_rm), paste0(names(sticker_rm[sticker_rm]), collapse = ", ")))
 
+# Coerce sticker sizes
 sticker_height <- stickers %>%
   map(image_info) %>%
   map_dbl("height") %>%
@@ -40,7 +41,27 @@ sticker_height <- stickers %>%
 stickers <- stickers %>%
   map(image_resize, paste0(sticker_width, "x", sticker_height, "!"))
 
+# Constuct canvas
 sticker_col_size <- ceiling(NROW(image_info(stickers))/(sticker_row_size-0.5))
-canvas <- image_blank(sticker_row_size*sticker_width, sticker_col_size*sticker_height)
+canvas <- image_blank(sticker_row_size*sticker_width, sticker_col_size*sticker_height/1.33526, "white")
 
-stickers[[1]]
+# Compute stickers by row
+row_lens <- rep(c(sticker_row_size,sticker_row_size-1), length.out=sticker_col_size)
+row_lens[length(row_lens)] <- row_lens[length(row_lens)]  - (length(stickers) - sum(row_lens))
+
+sticker_rows <- map2(row_lens, cumsum(row_lens),
+     ~ seq(.y-.x+1, by = 1, length.out = .x)) %>%
+  map(~ stickers[.x] %>%
+        invoke(c, .) %>%
+        image_append)
+
+# Add stickers to canvas
+canvas <- image_blank(sticker_row_size*sticker_width, sticker_col_size*sticker_height, "white")
+for(row in seq_along(sticker_rows)){
+  canvas <- image_composite(
+    canvas, sticker_rows[[row]],
+    offset = paste0("+", ((row-1)%%2)*sticker_width/2, "+", (row-1)*sticker_height/1.33526)
+  )
+}
+canvas
+image_write(canvas, "featurewall.png", format = "png")
